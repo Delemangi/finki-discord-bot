7ef09a685930f539dc7c32c187d2167738c23022
Delemangi <milev.stefan@gmail.com>
2022-08-27 17:53:30 +0200

    Change to 2 spaces

--- commands/link.ts
+++ commands/link.ts
@@ -2,18 +2,18 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { getAllOptions, getEmbedFromLink, getLink } from '../src/links.js';
 
 export const data = new SlashCommandBuilder()
-    .setName('link')
-    .setDescription('Get a link')
-    .addStringOption(option => option
-        .setName('keyword')
-        .setDescription('Keyword')
-        .setRequired(true)
-        .addChoices(...getAllOptions()));
+  .setName('link')
+  .setDescription('Get a link')
+  .addStringOption(option => option
+    .setName('keyword')
+    .setDescription('Keyword')
+    .setRequired(true)
+    .addChoices(...getAllOptions()));
 
 export async function execute (interaction: ChatInputCommandInteraction): Promise<void> {
-    const keyword = interaction.options.getString('keyword') ?? '';
-    const link = getLink(keyword);
-    const embed = getEmbedFromLink(link);
+  const keyword = interaction.options.getString('keyword') ?? '';
+  const link = getLink(keyword);
+  const embed = getEmbedFromLink(link);
 
-    await interaction.editReply({ embeds: [embed] });
+  await interaction.editReply({ embeds: [embed] });
 }
--- src/links.ts
+++ src/links.ts
@@ -4,23 +4,23 @@ import { readFileSync } from 'fs';
 const links: EmbedInfo[] = JSON.parse(readFileSync('./config/links.json', 'utf8'));
 
 export function getAllLinks (): string[] {
-    return links.map(link => link.name);
+  return links.map(link => link.name);
 }
 
 export function getAllOptions (): EmbedInfo[] {
-    return links.map(link => {
-        return { name: link.name.replaceAll('`', ''), value: link.name };
-    });
+  return links.map(link => {
+    return { name: link.name.replaceAll('`', ''), value: link.name };
+  });
 }
 
 export function getLink (keyword: string): EmbedInfo {
-    return links.find(link => link.name === keyword) ?? { name: 'No link found', value: 'No link found' };
+  return links.find(link => link.name === keyword) ?? { name: 'No link found', value: 'No link found' };
 }
 
 export function getEmbedFromLink (link: EmbedInfo): EmbedBuilder {
-    return new EmbedBuilder()
-        .setColor('Orange')
-        .setTitle(link.name)
-        .setDescription(link.value)
-        .setTimestamp();
+  return new EmbedBuilder()
+    .setColor('Orange')
+    .setTitle(link.name)
+    .setDescription(link.value)
+    .setTimestamp();
 }
