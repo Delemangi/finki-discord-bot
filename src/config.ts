6a8cb11fc2a19d2161c7a300f52340f1dc3728bb
Delemangi <milev.stefan@gmail.com>
2022-08-29 00:49:11 +0200

    Fix return type of config function

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ import { readFileSync } from 'fs';
 
 const config: Config = JSON.parse(readFileSync('./config/config.json', 'utf8'));
 
-export function getFromConfig<T extends keyof Config> (key: T): string {
+export function getFromConfig<T extends keyof Config> (key: T): Config[T] {
   return config[key];
 }
