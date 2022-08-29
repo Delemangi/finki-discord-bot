ca36aab4694eaa10260994f8b190427ccb7340cc
Delemangi <milev.stefan@gmail.com>
2022-08-29 10:24:52 +0200

    Add color statistics

---
+++ commands/color.ts
@@ -0,0 +1,29 @@
+import { ChatInputCommandInteraction, Role, SlashCommandBuilder } from 'discord.js';
+import { getFromConfig } from '../src/config.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('color')
+  .setDescription('Color')
+  .addSubcommand(command => command
+    .setName('statistics')
+    .setDescription('Get color statistics'));
+
+export async function execute (interaction: ChatInputCommandInteraction): Promise<void> {
+  const guild = interaction.guild;
+  const colorRoles = getFromConfig('colorRoles');
+
+  if (guild === null) {
+    interaction.editReply('You can only use this command in a server.');
+    return;
+  }
+
+  await guild.members.fetch();
+
+  const roles = colorRoles.map(role => guild.roles.cache.find(r => r.name === role)) as Role[];
+  const output = roles.map(role => `${role.name}: ${role.members.size}`);
+
+  // @ts-ignore
+  output.sort((a, b) => parseInt(b.split(':')[1].trim()) - parseInt(a.split(':')[1].trim()));
+
+  await interaction.editReply(output.join('\n'));
+}
--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
-const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages];
+const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers];
 const presence = { activities: [{ name: 'World Domination' }] };
 export const client = new Client({ intents, presence });
