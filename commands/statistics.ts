55d2d7e18e36aee202b2b173fd3c7b3351a43360
Delemangi <milev.stefan@gmail.com>
2022-09-20 01:09:25 +0200

    Refactor statistics

--- commands/statistics.ts
+++ commands/statistics.ts
@@ -1,17 +1,18 @@
 import {
   type ChatInputCommandInteraction,
   type Role,
-  SlashCommandBuilder
+  SlashCommandBuilder,
+  roleMention
 } from 'discord.js';
 import { getFromRoleConfig } from '../utils/config.js';
 import { isTextGuildBased } from '../utils/functions.js';
 
 export const data = new SlashCommandBuilder()
-  .setName('color')
+  .setName('statistics')
   .setDescription('Color')
   .addSubcommand((command) => command
-    .setName('statistics')
-    .setDescription('Get color statistics'));
+    .setName('color')
+    .setDescription('Get the number of members for each color'));
 
 export async function execute (interaction: ChatInputCommandInteraction): Promise<void> {
   const guild = interaction.guild;
@@ -25,10 +26,10 @@ export async function execute (interaction: ChatInputCommandInteraction): Promis
   await guild.members.fetch();
 
   const roles = colorRoles.map((role) => guild.roles.cache.find((r) => r.name === role)) as Role[];
-  const output = roles.map((role) => `${role.name}: ${role.members.size}`);
+  const output = roles.sort((a, b) => b.members.size - a.members.size).map((role) => `${roleMention(role.id)}: ${role.members.size}`);
 
-  // @ts-expect-error The split will always work
-  output.sort((a, b) => Number.parseInt(b.split(':')[1].trim()) - Number.parseInt(a.split(':')[1].trim()));
-
-  await interaction.editReply(output.join('\n'));
+  await interaction.editReply({
+    allowedMentions: { parse: [] },
+    content: output.join('\n')
+  });
 }
