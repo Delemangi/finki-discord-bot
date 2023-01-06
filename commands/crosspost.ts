939636cd79e4131e9e0db568d8949f1b5214a41f
Delemangi <milev.stefan@gmail.com>
2023-01-06 18:33:43 +0100

    Allow only admins to use crosspost command

--- commands/crosspost.ts
+++ commands/crosspost.ts
@@ -1,6 +1,7 @@
 import {
   type ChatInputCommandInteraction,
-  SlashCommandBuilder
+  SlashCommandBuilder,
+  PermissionsBitField
 } from 'discord.js';
 import { toggleCrossposting } from '../utils/crossposting.js';
 import { CommandsDescription } from '../utils/strings.js';
@@ -12,5 +13,11 @@ export const data = new SlashCommandBuilder()
   .setDescription(CommandsDescription[command]);
 
 export async function execute (interaction: ChatInputCommandInteraction): Promise<void> {
+  const permissions = interaction.member?.permissions as PermissionsBitField;
+  if (!permissions.has(PermissionsBitField.Flags.Administrator) && !permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+    await interaction.editReply('Оваа команда е само за администратори.');
+    return;
+  }
+
   await interaction.editReply(`Crossposting е ${toggleCrossposting() ? 'вклучено' : 'исклучено'}.`);
 }
