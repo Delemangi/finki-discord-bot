68947ffb7d01c275b2bea1800109420f48ac74d5
Delemangi <milev.stefan@gmail.com>
2023-01-06 19:33:11 +0100

    Add timestamp to about command

--- commands/about.ts
+++ commands/about.ts
@@ -13,7 +13,8 @@ const embed = new EmbedBuilder()
   .setColor(getFromBotConfig('color'))
   .setTitle('ФИНКИ Discord бот')
   .setThumbnail('https://cdn.discordapp.com/attachments/946729216152576020/1016773768938541106/finki-logo.png')
-  .setDescription(`Овој бот е развиен од <@198249751001563136> за потребите на Discord серверот на студентите на ФИНКИ. Ботот е open source и може да се најде на [GitHub](https://github.com/Delemangi/finki-discord-bot). Ако имате било какви прашања, предлози или проблеми, контактирајте не на Discord или на GitHub. \n\nНапишете ${inlineCode('/help')} за да ги видите сите достапни команди од ботот. Останатите функционалности се достапни на GitHub.`);
+  .setDescription(`Овој бот е развиен од <@198249751001563136> за потребите на Discord серверот на студентите на ФИНКИ. Ботот е open source и може да се најде на [GitHub](https://github.com/Delemangi/finki-discord-bot). Ако имате било какви прашања, предлози или проблеми, контактирајте нè на Discord или на GitHub. \n\nНапишете ${inlineCode('/help')} за да ги видите сите достапни команди од ботот. Останатите функционалности се достапни на GitHub.`)
+  .setTimestamp();
 
 export const data = new SlashCommandBuilder()
   .setName(command)
