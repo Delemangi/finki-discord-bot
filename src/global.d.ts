96e55302d57adc41c847c51cb0174f7000259dde
Delemangi <milev.stefan@gmail.com>
2023-02-07 06:00:44 +0100

    Add more years for course participants

--- src/global.d.ts
+++ src/global.d.ts
@@ -75,6 +75,11 @@ declare global {
   };
 
   type CourseParticipants = {
+    '2011/2012': number;
+    '2012/2013': number;
+    '2013/2014': number;
+    '2014/2015': number;
+    '2015/2016': number;
     '2016/2017': number;
     '2017/2018': number;
     '2018/2019': number;
