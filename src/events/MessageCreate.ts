a89768f51aa0253f71fe6dedc043b6511d3df1dc
Delemangi <milev.stefan@gmail.com>
2023-05-04 21:47:57 +0200

    Rename experience module

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -1,6 +1,6 @@
 import { getFromBotConfig } from '../utils/config.js';
 import { getCrossposting } from '../utils/crossposting.js';
-import { addExperience } from '../utils/leveling.js';
+import { addExperience } from '../utils/experience.js';
 import { logger } from '../utils/logger.js';
 import { type ClientEvents, Events, type Message } from 'discord.js';
 
