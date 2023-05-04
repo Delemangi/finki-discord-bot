c26f7ad13732b279956cf01dbbde5726a29e201d
Delemangi <milev.stefan@gmail.com>
2023-05-04 21:46:48 +0200

    Fix auto generation of primary key

--- src/entities/Experience.ts
+++ src/entities/Experience.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Experience {
-  @PrimaryColumn('uuid')
+  @PrimaryGeneratedColumn('uuid')
   public id!: string;
 
   @Column('text', { nullable: false })
