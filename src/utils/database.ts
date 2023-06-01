66570e6db0c03172ade8530b7e837bb98fcea040
Delemangi <milev.stefan@gmail.com>
2023-06-01 18:13:33 +0200

    Allow only VIP+ members to vote

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -382,10 +382,7 @@ export const createVipPoll = async (
   poll.open = false;
   poll.owner = client.user?.id ?? '';
   poll.options = [yes, no];
-  poll.roles = [
-    getFromBotConfig('roles').vipVoting,
-    getFromBotConfig('roles').admin,
-  ];
+  poll.roles = [getFromBotConfig('roles').vipVoting];
   poll.done = false;
   poll.threshold = threshold ?? 0.5;
 
