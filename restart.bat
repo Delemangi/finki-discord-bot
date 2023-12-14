82411e5fcbe4f675e3fbe01e3d3e2a38b345f4fa
Delemangi <milev.stefan@gmail.com>
2023-12-14 18:12:20 +0100

    Add restart scripts

---
+++ restart.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+set "PLATFORM=%~1"
+if not defined PLATFORM set "PLATFORM=linux/arm64"
+
+git pull
+docker-compose build --build-arg PLATFORM=%PLATFORM%
+docker-compose up -d
---
+++ restart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PLATFORM="${1:-linux/arm64}"
+
+git pull
+sudo docker-compose build --build-arg PLATFORM="$PLATFORM"
+sudo docker-compose up -d
