3969e336885e08d402daf983bd56e6acc479d607
Delemangi <milev.stefan@gmail.com>
2024-01-17 00:10:16 +0100

    Extend ESLint configuration

--- src/schemas/ConfigSchema.ts
+++ src/schemas/ConfigSchema.ts
@@ -1,37 +1,45 @@
 import { z } from 'zod';
 
-export const ConfigSchema = z.object({
-  buttonIdleTime: z.number(),
-  channels: z.object({
-    activity: z.string().nullable(),
-    commands: z.string().nullable(),
-    oath: z.string().nullable(),
-    polls: z.string().nullable(),
-    vip: z.string().nullable(),
-  }),
-  color: z.string(),
-  crosspostChannels: z.array(z.string()),
-  crossposting: z.boolean(),
-  ephemeralReplyTime: z.number(),
-  guild: z.string(),
-  leveling: z.boolean(),
-  roles: z.object({
-    admin: z.string().nullable(),
-    booster: z.string().nullable(),
-    contributor: z.string().nullable(),
-    council: z.string().nullable(),
-    fss: z.string().nullable(),
-    moderator: z.string().nullable(),
-    ombudsman: z.string().nullable(),
-    regular: z.string().nullable(),
-    veteran: z.string().nullable(),
-    vip: z.string().nullable(),
-  }),
-  temporaryVIPChannel: z.object({
-    cron: z.string(),
-    name: z.string(),
-    parent: z.string(),
-    position: z.number(),
-  }),
-  vipPause: z.boolean(),
-});
+export const ConfigSchema = z
+  .object({
+    buttonIdleTime: z.number(),
+    channels: z
+      .object({
+        activity: z.string().nullable(),
+        commands: z.string().nullable(),
+        oath: z.string().nullable(),
+        polls: z.string().nullable(),
+        vip: z.string().nullable(),
+      })
+      .strict(),
+    color: z.string(),
+    crosspostChannels: z.array(z.string()),
+    crossposting: z.boolean(),
+    ephemeralReplyTime: z.number(),
+    guild: z.string(),
+    leveling: z.boolean(),
+    roles: z
+      .object({
+        admin: z.string().nullable(),
+        booster: z.string().nullable(),
+        contributor: z.string().nullable(),
+        council: z.string().nullable(),
+        fss: z.string().nullable(),
+        moderator: z.string().nullable(),
+        ombudsman: z.string().nullable(),
+        regular: z.string().nullable(),
+        veteran: z.string().nullable(),
+        vip: z.string().nullable(),
+      })
+      .strict(),
+    temporaryVIPChannel: z
+      .object({
+        cron: z.string(),
+        name: z.string(),
+        parent: z.string(),
+        position: z.number(),
+      })
+      .strict(),
+    vipPause: z.boolean(),
+  })
+  .strict();
