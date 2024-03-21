90fd5d2110b0ab5f28c568fa9e8b0d2f0e7b7756
Delemangi <milev.stefan@gmail.com>
2024-03-21 01:32:55 +0100

    Increase level thresholds

--- src/utils/levels.ts
+++ src/utils/levels.ts
@@ -1,5 +1,5 @@
-export const ADMIN_LEVEL = 20;
-export const COUNCIL_LEVEL = 18;
+export const ADMIN_LEVEL = 25;
+export const COUNCIL_LEVEL = 20;
 export const REGULAR_LEVEL = 10;
 
-export const VIP_ADMIN_OVERRIDE_LEVEL = 18;
+export const VIP_ADMIN_OVERRIDE_LEVEL = 25;
