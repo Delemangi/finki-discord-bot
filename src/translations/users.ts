91ab4c2c3f473ae7f8e50b0be441c38d3b8dfa6c
Delemangi <milev.stefan@gmail.com>
2024-05-06 21:33:42 +0200

    Sort users by tags

--- src/translations/users.ts
+++ src/translations/users.ts
@@ -5,11 +5,14 @@ import { inlineCode, userMention } from 'discord.js';
 export const tagAndMentionUser = ({ tag, id }: PartialUser) =>
   `${inlineCode(tag)} (${userMention(id)})`;
 
-export const formatUsers = (label: string, users: PartialUser[]) =>
-  `# ${label} (${users.length})\n${
+export const formatUsers = (label: string, users: PartialUser[]) => {
+  users.sort((a, b) => a.tag.localeCompare(b.tag));
+
+  return `# ${label} (${users.length})\n${
     users.length === 0
       ? labels.none
       : users
           .map((user, index) => `${index}. ` + tagAndMentionUser(user))
           .join('\n')
   }`;
+};
