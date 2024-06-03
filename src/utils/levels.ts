17efa94b5dad9e88c24301900dc0f981801b75db
Delemangi <milev.stefan@gmail.com>
2024-06-03 19:50:03 +0200

    Increase required level for VIP override

--- src/utils/levels.ts
+++ src/utils/levels.ts
@@ -2,4 +2,4 @@ export const ADMIN_LEVEL = 25;
 export const COUNCIL_LEVEL = 20;
 export const REGULAR_LEVEL = 10;
 
-export const VIP_ADMIN_OVERRIDE_LEVEL = 25;
+export const VIP_ADMIN_OVERRIDE_LEVEL = 30;
