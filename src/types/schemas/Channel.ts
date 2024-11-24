da758f9d6477ee0169c5faf19a86b0512453758d
Delemangi <milev.stefan@gmail.com>
2024-11-24 18:47:24 +0100

    Rename enum value

--- src/types/schemas/Channel.ts
+++ src/types/schemas/Channel.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export enum Channel {
   Activity = 'activity',
   Boys = 'boys',
-  Council = 'polls',
+  Council = 'council',
   Girlies = 'girlies',
   Irregulars = 'irregulars',
   Logs = 'logs',
