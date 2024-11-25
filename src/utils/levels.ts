24fbf837ebdbfa8df87bc33fb39a7ea382ae8eac
Delemangi <milev.stefan@gmail.com>
2024-11-25 01:06:30 +0100

    Lower admin override level

--- src/utils/levels.ts
+++ src/utils/levels.ts
@@ -2,4 +2,4 @@ export const ADMIN_LEVEL = 25;
 export const COUNCIL_LEVEL = 20;
 export const REGULAR_LEVEL = 10;
 
-export const VIP_ADMIN_OVERRIDE_LEVEL = 30;
+export const ADMIN_OVERRIDE_LEVEL = 20;
