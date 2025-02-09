2b192a42a4307f0feca4f73663ef983e30213a94
Delemangi <milev.stefan@gmail.com>
2025-02-09 20:01:12 +0100

    Fix crash

--- src/lib/schemas/ClientEvent.ts
+++ src/lib/schemas/ClientEvent.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const AwaitableVoid = z.union([z.void(), z.promise(z.void())]);
 
 export const ClientEventSchema = z.object({
-  execute: z.function().args(z.array(z.unknown())).returns(AwaitableVoid),
+  execute: z.function().args(z.unknown()).returns(AwaitableVoid),
   name: z.string(),
   once: z.boolean().optional(),
 });
