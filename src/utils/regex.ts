2fdcae6672b9e7603c5c21600898e4dd72aa5597
Delemangi <milev.stefan@gmail.com>
2025-02-16 20:37:51 +0100

    Fix string replace regex errors

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,5 +1,5 @@
 export const LINK_REGEX = /https?:\/\/\S+\.\S+/u;
 export const USER_ID_REGEX = /(?<=<@)\d+(?=>)/u;
-export const EMOJI_REGEX = /<:\w+:\d+>/u;
-export const URL_REGEX = /\bhttps?:\/\/\S+/u;
+export const EMOJI_REGEX = /<:\w+:\d+>/gu;
+export const URL_REGEX = /\bhttps?:\/\/\S+/gu;
 export const POLL_IDENTIFIER_REGEX = /\[\w+-\d+\]/u;
