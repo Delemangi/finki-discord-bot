6af956fd9a75a52a1ce353667031c4a7be868d54
Delemangi <milev.stefan@gmail.com>
2025-03-15 22:36:07 +0100

    Update link schemas

--- src/lib/schemas/Link.ts
+++ src/lib/schemas/Link.ts
@@ -26,34 +26,34 @@ export const LinksSchema = z.array(LinkSchema);
 
 export type Link = z.infer<typeof LinkSchema>;
 
-export const CreateLinkSchema = z
-  .object({
-    description: z.string().optional(),
-    name: z.string(),
-    url: z.string(),
-    userId: z.string().optional(),
-  })
-  .transform((data) => ({
-    description: data.description,
-    name: data.name,
-    url: data.url,
-    user_id: data.userId,
-  }));
+export const CreateLinkSchema = z.object({
+  description: z.string().nullable(),
+  name: z.string(),
+  url: z.string(),
+  userId: z.string().nullable(),
+});
+
+export const PreparedCreateLinkSchema = CreateLinkSchema.transform((data) => ({
+  description: data.description,
+  name: data.name,
+  url: data.url,
+  user_id: data.userId,
+}));
 
 export type CreateLink = z.infer<typeof CreateLinkSchema>;
 
-export const UpdateLinkSchema = z
-  .object({
-    description: z.string().optional(),
-    name: z.string().optional(),
-    url: z.string().optional(),
-    userId: z.string().optional(),
-  })
-  .transform((data) => ({
-    description: data.description,
-    name: data.name,
-    url: data.url,
-    user_id: data.userId,
-  }));
+export const UpdateLinkSchema = z.object({
+  description: z.string().nullable(),
+  name: z.string().nullable(),
+  url: z.string().nullable(),
+  userId: z.string().nullable(),
+});
+
+export const PreparedUpdateLinkSchema = UpdateLinkSchema.transform((data) => ({
+  description: data.description,
+  name: data.name,
+  url: data.url,
+  user_id: data.userId,
+}));
 
 export type UpdateLink = z.infer<typeof UpdateLinkSchema>;
